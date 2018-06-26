5af82500e6a54a175cea14cbb40b553601a984af
Jenkins <jenkins@ip-172-31-61-216.us-east-2.compute.internal>
2018-06-26 11:33:34 +0000

    deploy

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 		include_once('assets/includes/_settings.php');
-		$site['page']['title'] = 'UAT';
+		$site['page']['title'] = 'DEV';
 		//$site['page']['articletitle'] = '';
 		$site['page']['current']['section'] = 'home';
 		//$site['page']['current']['page'] = '';
@@ -9,7 +9,8 @@
 		include_once('assets/includes/header.php');
 ?>
 <!-- START PAGE CONTENT -->
-<h1>-----UAT BRANCH----------</h1>
+<h1><h1>-----------DEV Branch---------</h1></h1>
+<h1><h1>-----------Commit2---------</h1></h1>
 <h1>Let East-West University help you find your way to an affordable, high-quality, multi-cultural educational experience.</h1>
 <p>
 You’ll find EWU in Chicago’s South Loop neighborhood, easily accessible by bus, train and subway. Your professors are experienced professionals, experts in their fields with the ability and willingness to provide the guidance you need to succeed. You’ll also find a financial aid department dedicated to helping you – through part-time employment, grants, scholarships, loans and more – pursue every available means to attend.
