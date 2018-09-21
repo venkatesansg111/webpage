2e6f0ae03fbe8a1e39b9174cde09eed14e07851f
gkaravindkumar <gkaravindkumar@gmail.com>
2018-09-21 15:18:22 +0530

    c1
    c1

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 ?>
 <!-- START PAGE CONTENT -->
 <h1>-----------Master Branch---------</h1>
-<h1>Let East-West University help you find your way to an affordable, high-quality, multi-cultural educational experience.</h1>
+<h1>Testtt</h1>
 <p>
 You’ll find EWU in Chicago’s South Loop neighborhood, easily accessible by bus, train and subway. Your professors are experienced professionals, experts in their fields with the ability and willingness to provide the guidance you need to succeed. You’ll also find a financial aid department dedicated to helping you – through part-time employment, grants, scholarships, loans and more – pursue every available means to attend.
 <br/><br/>
